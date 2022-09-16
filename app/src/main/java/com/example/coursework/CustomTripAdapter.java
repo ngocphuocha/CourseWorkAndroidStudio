b2028b0bd8048c2c7d57eceb2e29ac0fe66e25e2
ngocphuocha <phuoc04012000@gmail.com>
2022-09-16 08:13:32 +0700

    Update trip data

--- app/src/main/java/com/example/coursework/CustomTripAdapter.java
+++ app/src/main/java/com/example/coursework/CustomTripAdapter.java
@@ -1,33 +1,44 @@
 package com.example.coursework;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.coursework.Trips.UpdateTripActivity;
+
 import java.util.ArrayList;
 
 public class CustomTripAdapter extends RecyclerView.Adapter<CustomTripAdapter.MyViewHolder> {
+    Activity activity;
     Context context;
-    ArrayList trip_id, trip_name, trip_destination, trip_date_of_trip, trip_require_assessment;
+    ArrayList trip_id, trip_name, trip_destination, trip_date_of_trip, trip_require_assessment, trip_description;
 
     //Constructor
-    CustomTripAdapter(Context context,
-                      ArrayList trip_id,
-                      ArrayList trip_name,
-                      ArrayList trip_destination,
-                      ArrayList trip_date_of_trip,
-                      ArrayList trip_require_assessment) {
+    CustomTripAdapter(
+//            Activity activity,
+            Context context,
+            ArrayList trip_id,
+            ArrayList trip_name,
+            ArrayList trip_destination,
+            ArrayList trip_date_of_trip,
+            ArrayList trip_require_assessment,
+            ArrayList trip_description) {
+//        this.activity = activity;
         this.context = context;
         this.trip_id = trip_id;
         this.trip_name = trip_name;
         this.trip_destination = trip_destination;
         this.trip_date_of_trip = trip_date_of_trip;
         this.trip_require_assessment = trip_require_assessment;
+        this.trip_description = trip_description;
     }
 
     @NonNull
@@ -38,6 +49,7 @@ public class CustomTripAdapter extends RecyclerView.Adapter<CustomTripAdapter.My
         return new MyViewHolder(view);
     }
 
+    // Binding to the view
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.trip_id_txt.setText(String.valueOf(trip_id.get(position)));
@@ -45,15 +57,33 @@ public class CustomTripAdapter extends RecyclerView.Adapter<CustomTripAdapter.My
         holder.trip_destination_txt.setText(String.valueOf(trip_destination.get(position)));
         holder.trip_date_txt.setText(String.valueOf(trip_date_of_trip.get(position)));
         holder.trip_require_txt.setText(String.valueOf(trip_require_assessment.get(position)));
+
+        // On click in the layout of trip data row
+        holder.tripLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, UpdateTripActivity.class);
+                intent.putExtra("trip_id", String.valueOf(trip_id.get(position)));
+                intent.putExtra("trip_name", String.valueOf(trip_name.get(position)));
+                intent.putExtra("trip_destination", String.valueOf(trip_destination.get(position)));
+                intent.putExtra("trip_date", String.valueOf(trip_date_of_trip.get(position)));
+                intent.putExtra("trip_requireAssessment", String.valueOf(trip_require_assessment.get(position)));
+                intent.putExtra("trip_description", String.valueOf(trip_description.get(position)));
+                context.startActivity(intent);
+            }
+        });
     }
 
+    // Loop by size of item
     @Override
     public int getItemCount() {
         return trip_id.size();
     }
 
+    // Get reference
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView trip_id_txt, trip_name_txt, trip_destination_txt, trip_date_txt, trip_require_txt;
+        LinearLayout tripLinearLayout;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -62,7 +92,8 @@ public class CustomTripAdapter extends RecyclerView.Adapter<CustomTripAdapter.My
             trip_destination_txt = itemView.findViewById(R.id.trip_destination_txt);
             trip_date_txt = itemView.findViewById(R.id.trip_date_txt);
             trip_require_txt = itemView.findViewById(R.id.trip_require_txt);
-
+            // Get reference linear layout
+            tripLinearLayout = itemView.findViewById(R.id.tripLinearLayout);
         }
     }
 }
---
+++ app/src/main/java/com/example/coursework/Trips/UpdateTripActivity.java
@@ -0,0 +1,239 @@
+package com.example.coursework.Trips;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.example.coursework.DatabaseHelper;
+import com.example.coursework.MainActivity;
+import com.example.coursework.R;
+import com.example.coursework.databinding.ActivityUpdateTripBinding;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UpdateTripActivity extends AppCompatActivity {
+    Activity activity;
+    ActivityUpdateTripBinding binding;
+    RadioButton require_input_radio;
+    String id, nameText, destinationText, dateOfTripText, requireAssessmentText, descriptionText;
+    boolean isValid = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUpdateTripBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Check valid on focus input text
+        nameFocusListener();
+        destinationFocusListener();
+        dateOfTripFocusListener();
+        descriptionFocusListener();
+
+        // First we call this
+        getAndSetIntentData();
+        // Set actionbar after getAndSetIntentData method
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setTitle(nameText);
+        }
+
+        // Get require assessment input radio text
+        binding.requireAssessmentGroupUpdate.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                require_input_radio = binding.getRoot().findViewById(binding.requireAssessmentGroupUpdate.getCheckedRadioButtonId());
+                requireAssessmentText = require_input_radio.getText().toString();
+            }
+        });
+
+
+        // On click update button
+        binding.updateTripButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitForm();
+            }
+        });
+    }
+
+    private void submitForm() {
+        if (!checkValid()) {
+            Toast.makeText(this, "Please fulfilled again!", Toast.LENGTH_SHORT).show();
+        } else {
+            DatabaseHelper db = new DatabaseHelper(UpdateTripActivity.this);
+            db.updateTrip(id, nameText, destinationText, dateOfTripText, requireAssessmentText, descriptionText);
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    private void getAndSetIntentData() {
+        if (getIntent().hasExtra("trip_id")
+                && getIntent().hasExtra("trip_name")
+                && getIntent().hasExtra("trip_destination")
+                && getIntent().hasExtra("trip_date")
+                && getIntent().hasExtra("trip_requireAssessment")
+                && getIntent().hasExtra("trip_description")) {
+            // Getting data from intent and setting data for edit text
+            id = getIntent().getStringExtra("trip_id");
+            binding.nameEditTextUpdate.setText(getIntent().getStringExtra("trip_name"));
+            binding.destinationEditTextUpdate.setText(getIntent().getStringExtra("trip_destination"));
+            binding.dateOfTripEditTextUpdate.setText(getIntent().getStringExtra("trip_date"));
+            binding.descriptionEditTextUpdate.setText(getIntent().getStringExtra("trip_description"));
+
+            Bundle extras = getIntent().getExtras();
+
+            // Set textValue
+            nameText = extras.getString("trip_name");
+            destinationText = extras.getString("trip_destination");
+            dateOfTripText = extras.getString("trip_date");
+            descriptionText = extras.getString("trip_description");
+
+            // get and set radio
+            requireAssessmentText = extras.getString("trip_requireAssessment");
+
+            if (requireAssessmentText.equals("Yes")) {
+                binding.requireAssessmentGroupUpdate.check(R.id.radio_yes_button_update);
+            } else {
+                binding.requireAssessmentGroupUpdate.check(R.id.radio_no_button_update);
+            }
+
+            require_input_radio = binding.getRoot().findViewById(binding.requireAssessmentGroupUpdate.getCheckedRadioButtonId());
+            requireAssessmentText = require_input_radio.getText().toString();
+        } else {
+            Toast.makeText(this, "No data intent.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void nameFocusListener() {
+        binding.nameEditTextUpdate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    binding.nameContainerUpdate.setHelperText(validName());
+                }
+            }
+        });
+    }
+
+    private String validName() {
+        nameText = binding.nameEditTextUpdate.getText().toString();
+
+        if (nameText.isEmpty()) {
+            return "Required*";
+        }
+        return "";
+    }
+
+    private void destinationFocusListener() {
+        binding.destinationEditTextUpdate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    binding.destinationContainerUpdate.setHelperText(validDestination());
+                }
+            }
+        });
+    }
+
+    private String validDestination() {
+        destinationText = binding.destinationEditTextUpdate.getText().toString();
+
+        if (destinationText.isEmpty()) {
+            return "Required*";
+        }
+
+        return "";
+    }
+
+    private void dateOfTripFocusListener() {
+        binding.dateOfTripEditTextUpdate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    binding.dateOfTripContainerUpdate.setHelperText(validDateOfTrip());
+                }
+            }
+        });
+    }
+
+    private String validDateOfTrip() {
+        dateOfTripText = binding.dateOfTripEditTextUpdate.getText().toString();
+
+        // https://www.w3schools.com/java/java_regex.asp regex document reference
+        // Validate a format is dd/mm/yyyy
+        Pattern pattern = Pattern.compile("^([0-2][0-9]|(3)[0-1])(\\/)(((0)[0-9])|((1)[0-2]))(\\/)\\d{4}$"); // https://www.regextester.com/99555
+        Matcher matcher = pattern.matcher(dateOfTripText);
+        boolean matchFound = matcher.find(); // Check if match pattern
+
+        if (dateOfTripText.isEmpty()) {
+            return "Required*";
+        } else if (!matchFound) {
+            return "Invalid format";
+        } else {
+            return "";
+        }
+    }
+
+    private void descriptionFocusListener() {
+        binding.descriptionEditTextUpdate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    binding.descriptionContainerUpdate.setHelperText(validDescription());
+                }
+            }
+        });
+    }
+
+    private String validDescription() {
+        descriptionText = binding.descriptionEditTextUpdate.getText().toString();
+
+        if (descriptionText.isEmpty()) {
+            return "Required*";
+        }
+
+        return "";
+    }
+
+    public boolean checkValid() {
+        nameText = binding.nameEditTextUpdate.getText().toString();
+        if (nameText.isEmpty()) {
+            isValid = false;
+        }
+
+        destinationText = binding.destinationEditTextUpdate.getText().toString();
+        if (destinationText.isEmpty()) {
+            isValid = false;
+        }
+
+        dateOfTripText = binding.dateOfTripEditTextUpdate.getText().toString();
+        Pattern pattern = Pattern.compile("^([0-2][0-9]|(3)[0-1])(\\/)(((0)[0-9])|((1)[0-2]))(\\/)\\d{4}$"); // https://www.regextester.com/99555
+        Matcher matcher = pattern.matcher(dateOfTripText);
+        boolean matchFound = matcher.find(); // Check if match pattern
+        if (dateOfTripText.isEmpty()) {
+            isValid = false;
+        }
+        if (!matchFound) {
+
+            isValid = true;
+        }
+
+        descriptionText = binding.descriptionEditTextUpdate.getText().toString();
+        if (descriptionText.isEmpty()) {
+            isValid = false;
+        }
+
+        return isValid;
+    }
+}
